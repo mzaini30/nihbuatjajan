fdd2770a809b6891e59c6f7b9ea7e192293e18c6
mzaini30 <muhzaini30@gmail.com>
2024-07-10 15:47:07 +0800

    upload

--- src/NihBuatJajan.svelte
+++ src/NihBuatJajan.svelte
@@ -1,12 +1,43 @@
 <script>
+  import { acak } from "kumpulan-tools";
   const { username } = $props();
+  let donasi = [];
+  let tampil = $state("");
+  let triggerTampil = $state(false);
   async function dapatkan() {
     let halaman = await fetch(
       `https://cors.mabaiz.web.id/?link=https://www.nihbuatjajan.com/${username}`,
     );
     halaman = await halaman.text();
     let domHalaman = new DOMParser().parseFromString(halaman, "text/html");
-    console.log(domHalaman);
+    let donation = domHalaman.querySelectorAll(".donation");
+    if (donation.length > 0) {
+      for (let x of donation) {
+        donasi = [...donasi, x.querySelector(".donator").innerHTML.trim()];
+      }
+      tampil = donasi[0];
+      triggerTampil = true;
+    }
   }
   dapatkan();
 </script>
+
+{#if triggerTampil}
+  <div class="alert alert-success flex justify-between" role="alert">
+    <div>
+      {@html tampil}
+    </div>
+    <button
+      onclick={() => (triggerTampil = false)}
+      type="button"
+      class="btn-close"
+      data-bs-dismiss="alert"
+      aria-label="Close"
+    ></button>
+  </div>
+{/if}
+<a
+  href="https://www.nihbuatjajan.com/{username}"
+  class="fixed bottom-2 right-2 btn btn-primary"
+  target="_blank">Support</a
+>
